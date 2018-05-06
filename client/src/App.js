fdf73b0fd2e222b8fdbf9a5bfb417714903025b0
Elisha Terada <elishaterada@gmail.com>
2018-05-05 23:02:14 -0700

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = { time: null }
+
+  componentDidMount() {
+    this.ping();
+  }
+
+  ping = () => {
+    fetch('/api/ping')
+      .then(res => res.json())
+      .then(ping => this.setState({ time: ping.time }))
+  }
+
+  render() {
+    const { time } = this.state;
+
+    return (
+      <div className="App">
+        { time }
+      </div>
+    );
+  }
+}
+
+export default App;
